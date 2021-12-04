4c5b508136d24f7423a36a28c095e4621841c776
Jeremy Leonard <jeremy@coyotetrackz.com>
2021-12-03 19:26:27 -0500

    small update to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
 #! /usr/bin/env python3
 
+# ---------------------------------------------------------------- #
+#  setup.py                                                        #
+# ---------------------------------------------------------------- #
+#  This setup script assumes that the bash shell is being used,    #
+#  and will setup the virtual environment along with getting the   #
+#  necessary libraries to make this tool work properly.            #
+# ---------------------------------------------------------------- #
+
+from os import system
+
 if __name__ == '__main__':
   pass
 
